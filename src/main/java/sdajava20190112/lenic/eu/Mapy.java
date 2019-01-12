277846b6233fce14dc3485115b3a35bed1dfa02d
Lukasz Kapiec <lkapiec@gmail.com>
2019-01-12 10:49:05 +0100

    Set tests

---
+++ src/main/java/sdajava20190112/lenic/eu/Mapy.java
@@ -0,0 +1,30 @@
+package sdajava20190112.lenic.eu;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Mapy {
+
+    public static void main(String [] args)
+    {
+        Map<String , String> pairsMap = new HashMap<String, String>();
+
+        pairsMap.put("Marcin", "Adela");
+        pairsMap.put("Marek",  "Magda");
+
+        Map<String , String> otherPairsMap = new HashMap<String, String>();
+
+        otherPairsMap.put("Marek", "Ewa");
+        otherPairsMap.put("Adam",  "Ewa");
+
+
+        pairsMap.putAll(otherPairsMap);
+
+        System.out.println(pairsMap.get("Marek"));
+        System.out.println(pairsMap.remove("Marek"));
+        System.out.println(pairsMap.size());
+        System.out.println(pairsMap.isEmpty());
+        System.out.println(pairsMap.containsKey("Jan"));
+        System.out.println(pairsMap.containsValue("Adela"));
+    }
+}
---
+++ src/main/java/sdajava20190112/lenic/eu/Sety.java
@@ -0,0 +1,31 @@
+package sdajava20190112.lenic.eu;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Sety {
+
+    public static void main(String...args)
+    {
+        Set<String> setWithNames = new HashSet<String>();
+
+        setWithNames.add("Marcin");
+        setWithNames.add("Marek");
+        setWithNames.add("Marcin");
+
+
+        Set<String> otherSet = new HashSet<String>();
+
+        setWithNames.add("Zenon");
+        setWithNames.add("Marek");
+
+        setWithNames.addAll(otherSet);
+
+        System.out.println(setWithNames.isEmpty());
+        System.out.println(setWithNames.size());
+        System.out.println(setWithNames.contains("Marcin"));
+        System.out.println(setWithNames.remove("Janusz"));
+
+    }
+
+}
